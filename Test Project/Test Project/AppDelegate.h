14fab7507b6829625aaf268214e31ab38a0d1658
Sidney Eubanks <sidneyeubanks@icloud.com>
2014-11-25 11:20:35 -0800

    Test Commit

---
+++ Test Project/Test Project/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test Project
+//
+//  Created by apple on 11/25/14.
+//  Copyright (c) 2014 Sidney Eubanks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
